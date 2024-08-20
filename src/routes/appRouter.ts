fa2ceb72a101cb28dfc1e692af0b615da77e3e6a
Marina Falcone <marina.falcone@dci-student.org>
2024-08-20 14:59:36 +0200

    reviewed feedback

--- src/routes/appRouter.ts
+++ src/routes/appRouter.ts
@@ -1,7 +1,3 @@
-// appRouter
-// main roue handler
-
-// Imports =========================================
 import { Router } from "express";
 import testRoutes from "./testRoutes.js";
 import userRoutes from "./userRoutes.js";
@@ -10,22 +6,13 @@ import calendarRoutes from './calendarRouter.js';
 import sessionRoutes from './sessionRouter.js';
 import feedbackRoutes from './feedbackRouter.js';
 
-// Routes ==========================================
-
 const appRouter = Router();
 
 appRouter.use("/test", testRoutes);
-
 appRouter.use("/user", userRoutes);
-
 appRouter.use("/skill", skillRoutes);
-
 appRouter.use('/calendar', calendarRoutes);
-
-appRouter.use('/session', sessionRoutes);
-
+appRouter.use('/session', sessionRoutes); 
 appRouter.use('/feedback', feedbackRoutes);
-
-// Exports =========================================
 
 export default appRouter;
