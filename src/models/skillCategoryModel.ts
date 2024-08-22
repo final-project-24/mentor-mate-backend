624d947ff63d0892485f6d72219b3b49245d8b46
Jakub <derivatives_four@protonmail.com>
2024-08-22 13:42:01 +0200

    feat: add skillCategory feature
    - Added `skillCategoryController.ts` to handle requests related to skill categories.
    - Introduced `paginationParamsValidationChain.ts` to validate pagination parameters for relevant routes.
    - Created `skillCategoryValidationChain.ts` for skill category-specific validation logic.
    - Implemented the `skillCategoryModel.ts` to define the schema for skill categories.
    - Modified `appRouter.ts` to include routes for `skillCategory` and the upcoming `protoSkill` routes.
    - Added `skillCategoryRouter.ts` to handle routing for the `skillCategory` module.
    
    This commit sets up the skill category feature and prepares for the inclusion of protoSkill in a future commit.

---
+++ src/models/skillCategoryModel.ts
@@ -0,0 +1,85 @@
+import { Schema, Model, model } from "mongoose";
+import capitalizeFirstChar from "../utils/capitalizeFirstChar.js";
+
+interface ISkillCategory extends Document {
+  skillCategoryTitle: string
+  skillCategoryTitleLower: string
+  skillCategoryDescription: string
+}
+
+interface ISkillCategoryModel extends Model<ISkillCategory> {
+  skillCategoryAlreadyExistsByTitle(categoryTitle: string): Promise<void>
+  validateCategoryChanges(data: object, id: string): Promise<void>
+}
+
+const skillCategorySchema = new Schema<ISkillCategory>({
+  skillCategoryTitle: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  // this title prop is used internally in BE for comparison purposes
+  skillCategoryTitleLower: {
+    type: String,
+    unique: true,
+    select: false
+  },
+  skillCategoryDescription: {
+    type: String
+  }
+}, {
+  timestamps: true
+})
+
+// ! static method to check for category title conflict
+skillCategorySchema.statics.skillCategoryAlreadyExistsByTitle = async function (categoryTitle) {
+  const categoryExists = await this.findOne({skillCategoryTitleLower: categoryTitle})
+
+  if (categoryExists)
+    throw new Error('Category with this title already exists!')
+}
+
+// ! static method to validate category edit
+skillCategorySchema.statics.validateCategoryChanges = async function (data, id) {
+  // fetch category to modify
+  const existingCategory = await this.findById(id)
+
+  if (!existingCategory)
+    throw new Error('Provided category ID does not match any category')
+
+  // check for other categories with the same title
+  const categoryExistsByTitle = await this.findOne({skillCategoryTitleLower: data.skillCategoryTitle.toLowerCase()})
+
+  if (categoryExistsByTitle)
+    throw new Error('Category with this title already exists!')
+
+  // check if updated category differs from original skill
+  const isDifferent = Object.keys(data).some(key => existingCategory[key] !== data[key])
+
+  if (!isDifferent)
+    throw new Error('No category changes were detected!')
+}
+
+// ! pre hook
+skillCategorySchema.pre<ISkillCategory>('save', function (next) {
+  this.skillCategoryTitle = capitalizeFirstChar(this.skillCategoryTitle)
+  this.skillCategoryTitleLower = this.skillCategoryTitle.toLowerCase()
+  
+  if (this.skillCategoryDescription)
+    this.skillCategoryDescription = capitalizeFirstChar(this.skillCategoryDescription)
+
+  next()
+})
+
+// ! set hook
+// excludes skillCategoryTitleLower prop from response object when using .json() method
+skillCategorySchema.set('toJSON', {
+  transform: function (_, ret) {
+    delete ret.skillCategoryTitleLower
+    return ret
+  }
+})
+
+const skillCategoryModel = model<ISkillCategory, ISkillCategoryModel>('skill_category', skillCategorySchema)
+
+export default skillCategoryModel
